90cfdac70c271a3146da467eb41a333ad71c8e98
galinagr <galina.gryaznova@thomascookonline.com>
2016-09-19 10:22:04 +0300

    1st module HW

---
+++ src/src/SecondClass.java
@@ -0,0 +1,8 @@
+/**
+ * This method is printing a message
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ src/src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * This method is printing a message
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
