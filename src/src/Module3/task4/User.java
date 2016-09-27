849d02410acbef2493d346eca04538dcb374b2db
galinagr <galina.gryaznova@thomascookonline.com>
2016-09-27 13:07:53 +0300

    3st module HW

--- src/src/Module3/task4/User.java
+++ src/src/Module3/task4/User.java
@@ -1,16 +1,101 @@
 package Module3.task4;
 
 public class User {
-    //String name;
-    //int balance;
-    //Date startOfWork;
-    //
-    //
-    //
-    //
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
 
-    //a few constructors
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    private int getBalance() {
+        return balance;
+    }
 
+    private int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    private String getCompanyName() {
+        return companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void paidSalary() {
+        balance += balance + salary;
+    }
+
+    public double withdraw(int summ) {
+        double result = 0;
+        if (summ < 1000) {
+            result = getBalance() - summ - summ * 0.05;
+        } else if (summ < 1000) {
+            result = getBalance() - summ - summ * 0.1;
+        }
+        return result;
+    }
+
+    public int companyNameLenght() {
+        String s = getCompanyName();
+        int charCount = 0;
+        char temp;
+
+        for (int i = 0; i < s.length(); i++) {
+            temp = s.charAt(i);
+
+            if (temp != ' ')
+                charCount++;
+        }
+        return charCount;
+    }
+
+    public int monthIncreaser(int addMonth) {
+        int c = getMonthsOfEmployment();
+        c+= addMonth;
+        return c;
+    }
 }
